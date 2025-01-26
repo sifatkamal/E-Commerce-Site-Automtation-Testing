144da2919114097534a692be4e192a39e4a3fd0c
Md. Sifat Kamal <md.sifat.kamal@g.bracu.ac.bd>
2025-01-26 20:56:12 +0600

    purchased jeans product, Added Message

--- cypress/e2e/test1.js
+++ cypress/e2e/test1.js
@@ -13,7 +13,6 @@ describe('SignUp', function(){
         cy.get('[data-qa="days"]').select('25')
         cy.get('[data-qa="months"]').select('9')
         cy.get('[data-qa="years"]').select('2001')
-
         cy.get('[data-qa="first_name"]').type('Dexter')
         cy.get('[data-qa="last_name"]').type('Morgan')
         cy.get('[data-qa="address"]').type('Dhaka, Bangladesh')
@@ -21,11 +20,60 @@ describe('SignUp', function(){
         cy.get('[data-qa="city"]').type('Dhaka')
         cy.get('[data-qa="zipcode"]').type('1217')
         cy.get('[data-qa="mobile_number"]').type('123456789')
+        cy.get('button[data-qa="create-account"]').click()
+        cy.get('[data-qa="continue-button"]').click()
 
-        cy.get('.nav > :nth-child(2) > a').click()
+
 
     })
 
 })
+
+
+
+
+
+
+
+describe('All Jeans Product', function(){
+
+    it('redirect to jeans section', function(){
+
+
+        cy.visit('https://automationexercise.com/category_products/6');
+        cy.get('i[class="fa fa-plus"]').eq(1).click()
+        cy.get('#Men > .panel-body > ul > :nth-child(2) > a').click()
+        cy.get(':nth-child(3) > .product-image-wrapper > .choose > .nav > li > a').click()
+        cy.get('#quantity').clear().should('have.value', '')
+        cy.get('#quantity').type('2')
+        cy.get(':nth-child(5) > .btn').click()
+        cy.wait(2000)
+        cy.get('u').click()
+        cy.get('.col-sm-6 > .btn').click()
+
+
+    })
+
+})
+
+
+
+describe('Product', function(){
+
+    it('redirect to jeans section', function(){
+
+        cy.visit('https://automationexercise.com/contact_us');
+        cy.get('[data-qa="name"]').type('Dexter')
+        cy.get('[data-qa="email"]').type('noobgamer201971@gmail.com')
+        cy.get('[data-qa="subject"]').type('xyz')
+        cy.get('[data-qa="message"]').type('xyz')
+
+
+
+    })
+
+})
+
+
 
 
